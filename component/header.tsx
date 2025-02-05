c08ebb0f067f3d76b0560ec45e43566ef73e35ec
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-02-05 09:08:05 +0900

    修正

--- component/header.tsx
+++ component/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Search, X, Menu } from "lucide-react";
+import { X, Menu } from "lucide-react";
 import Link from "next/link";
 
 const Header = () => {
