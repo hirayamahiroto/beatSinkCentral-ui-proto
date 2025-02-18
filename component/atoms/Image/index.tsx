89c05edc26bc83c1c2edbd40e4914dc7c761c8db
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-02-18 23:10:37 +0900

    命名の修正

--- component/atoms/Image/index.tsx
+++ component/atoms/Image/index.tsx
@@ -1,13 +1,14 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import classes from "./index.module.css";
-
+import { cn } from "./../../../libs";
 type ImageProps = {
   src: string;
   alt: string;
+  className?: string;
 };
 
-const Image = ({ src, alt }: ImageProps) => {
-  return <LazyLoadImage src={src} alt={alt} className={classes.image} />;
+const Image = ({ src, alt, className }: ImageProps) => {
+  return <LazyLoadImage src={src} alt={alt} className={cn(classes.image, className)} />;
 };
 
-export default Image;
+export { Image };
