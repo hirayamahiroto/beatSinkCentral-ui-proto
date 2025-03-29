f43199c08b6883b0314e74864eb6c94cb6fd579f
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-03-29 16:43:53 +0900

    ♻️ importを修正

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -1,4 +1,4 @@
-import AdminCalendarPage from "../../../../packages/ui/components/pages/AdminCalendarPage";
+import AdminCalendarPage from "../../../../packages/ui/design-system/components/pages/AdminCalendarPage";
 
 export default function AdminCalendar() {
   return <AdminCalendarPage />;
