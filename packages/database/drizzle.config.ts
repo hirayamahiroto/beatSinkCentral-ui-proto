269157d4fcfd8c7debdc3951650b1c9aa71b3333
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-04-03 22:54:29 +0900

    📦 migration実行時の出力先を変更しました。

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./src/schema",
-  schema: "./src/schema/index.ts",
+  out: "./drizzle/migrations",
+  schema: "./src/schema/*.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
