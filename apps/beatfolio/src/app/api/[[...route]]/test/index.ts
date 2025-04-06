bad82d329caf3c48aa064f2f96eba864942cb8ef
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-04-06 19:01:32 +0900

    ✨ テスト用のfetcherを作成しました。

--- apps/beatfolio/src/app/api/[[...route]]/test/index.ts
+++ apps/beatfolio/src/app/api/[[...route]]/test/index.ts
@@ -7,19 +7,11 @@ const messageSchema = z.object({
   message: z.string(),
 });
 
-const app = new Hono().get(
-  "/",
-  zValidator("json", messageSchema, (result, c) => {
-    if (!result.success) {
-      return c.text("Invalid!", 400);
-    }
-  }),
-  async (c) => {
-    const client = createApiServerClient();
-    const res = await client.api.test.$get();
+const app = new Hono().get("/", async (c) => {
+  const client = createApiServerClient();
+  const res = await client.api.test.$get();
 
-    return c.json({ message: "Hello World" });
-  }
-);
+  return c.json({ message: "Hello World" });
+});
 
 export default app;
