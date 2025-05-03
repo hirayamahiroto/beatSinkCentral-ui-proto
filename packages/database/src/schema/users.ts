22d17747455b26298ef0db0b49e27a980183976f
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-05-03 15:18:26 +0900

    ✨ usersTableにカラムを追加 -> password

--- packages/database/src/schema/users.ts
+++ packages/database/src/schema/users.ts
@@ -14,6 +14,7 @@ export const usersTable = pgTable("users", {
     .references(() => userRolesTable.id),
   name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
