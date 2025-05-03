6752942c385ea828db3e2f6770a8bdb1675bb3e3
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-05-03 15:22:25 +0900

    Feature/table redesing (#60)
    * 🔥 一旦、不要なテーブルを削除しました。
    
    * ✨ userRoles及びUserのスキーマを修正
    
    * 🔥 players参照を削除
    
    * ✨ ジャーナルファイルを追加
    
    * ✨ ignoreにenv追加
    
    * 🔧  migration関連のファイルを追加しました。
    
    * ✨ usersTableにカラムを追加 -> emailとname
    
    * 🔥不要なコードを削除
    
    * 🔥 backendのディレクトリ不要のため削除
    
    * 🔥 backendのディレクトリ不要のため削除
    
    * ✨ usersTableにカラムを追加 -> password
    
    * ✨ マイグレーション後のファイルを追加しました。

--- packages/database/src/schema/users.ts
+++ packages/database/src/schema/users.ts
@@ -5,7 +5,6 @@ import {
   createSelectSchema,
   createUpdateSchema,
 } from "drizzle-zod";
-import { playersTable } from "./players";
 import { userRolesTable } from "./userRoles";
 
 export const usersTable = pgTable("users", {
@@ -13,14 +12,14 @@ export const usersTable = pgTable("users", {
   roleId: uuid("role_id")
     .notNull()
     .references(() => userRolesTable.id),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
 export const usersRelations = relations(usersTable, ({ one }) => ({
-  player: one(playersTable, {
-    fields: [usersTable.id],
-    references: [playersTable.userId],
-  }),
   role: one(userRolesTable, {
     fields: [usersTable.roleId],
     references: [userRolesTable.id],
