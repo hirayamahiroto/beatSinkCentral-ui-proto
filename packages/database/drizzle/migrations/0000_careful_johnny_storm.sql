e5f5b1587ebc8fc11356fcd67261521f9dc8df6a
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-05-03 15:20:48 +0900

    ✨ マイグレーション後のファイルを追加しました。

---
+++ packages/database/drizzle/migrations/0000_careful_johnny_storm.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "users" ADD COLUMN "name" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "email" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "password" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_email_unique" UNIQUE("email");
