ee7084c9ce5263817892dc4dd82573c10953ef8e
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-05-24 19:03:23 +0900

    page.tsxを仮実装

--- apps/beatfolio/src/app/player/page.tsx
+++ apps/beatfolio/src/app/player/page.tsx
@@ -1,5 +1,74 @@
-import PlayerPage from "@ui/design-system/components/pages/PlayerPage";
+import {
+  PlayerPage,
+  PlayerDetailProps,
+} from "@ui/design-system/components/pages/PlayerPage";
 
-export default function Player() {
-  return <PlayerPage />;
+type SignatureSound = {
+  title: string;
+  description: string;
+  audio?: string;
+};
+
+type Badge = {
+  text: string;
+  primary: boolean;
+  icon: string;
+};
+
+type Stat = {
+  value: string;
+  label: string;
+};
+
+type Story = {
+  tittle: string;
+  description: string;
+};
+
+type Skill = {
+  name: string;
+  description: string;
+};
+
+type Performance = {
+  title: string;
+  views: string;
+  duration: string;
+  image: string;
+};
+
+type Activity = {
+  date: string;
+  title: string;
+  description: string;
+};
+
+type PlayerData = {
+  id: number;
+  image: string;
+  name: string;
+  tagline: string;
+  signatureSound?: SignatureSound;
+  badges: Badge[];
+  stats: Stat[];
+  story: Story[];
+  skills: Skill[];
+  performance: Performance[];
+  activities: Activity[];
+};
+
+const players: PlayerData[] = [];
+
+type PlayerComponentProps = {
+  id: number;
+};
+
+export default function Player({ id }: PlayerComponentProps) {
+  const playerData = players.find((player) => player.id === id);
+
+  if (!playerData) {
+    return <div>Player not found</div>;
+  }
+
+  return <PlayerPage playerData={playerData} />;
 }
--- apps/beatfolio/src/app/players/page.tsx
+++ apps/beatfolio/src/app/players/page.tsx
@@ -1,5 +1,6 @@
 import PlayersPage from "@ui/design-system/components/pages/PlayersPage";
+import { players } from "../../../../../packages/data/players";
 
 export default function Players() {
-  return <PlayersPage />;
+  return <PlayersPage players={players} />;
 }
